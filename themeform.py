b6b233e5187679aa15e9f0a0fa682f65855c3921
smitelimit <smitelimit@mail.ru>
2023-02-28 16:05:37 +0300

    tkinter upgrade

---
+++ themeform.py
@@ -0,0 +1,60 @@
+import json, sys, os, time
+
+
+def jsonload():
+    if getattr(sys, 'frozen', False):
+        f = open(os.path.join(sys._MEIPASS, 'theme.json'), encoding="utf-8")
+    else:
+        f = open('theme.json', encoding="utf-8")
+    return json.load(f)
+
+
+def savetheme(data, style, themename="ТемаТест"):
+    for index, widgettype in enumerate(data['WidgetStyles']['$values']):
+        data['WidgetStyles']['$values'][index]['Title']['Background']['Color']['Color'] = style[0]
+        data['WidgetStyles']['$values'][index]['Background']['Color']['Color'] = style[1]
+
+    data['WidgetStyles']['$values'][15]['FillColor'] = style[2]
+
+    data['Name'] = themename
+
+    with open(f'{themename}.json', 'w', encoding='utf-8') as fp:
+        json.dump(data, fp, ensure_ascii=False, indent='\t')
+
+# try:
+#     # if getattr(sys, 'frozen', False):
+#     #     f = open(os.path.join(sys._MEIPASS, 'theme.json'), encoding="utf-8")
+#     # else:
+#     #     f = open('theme.json', encoding="utf-8")
+#     #
+#     # data = json.load(f)
+#     data = jsonload()
+#     themename = input("Введите название новой темы: ")
+#     style = []
+#     for el, col in zip(['цвет фона заголовка', 'цвет фона', 'цвет спидометра'], ['249E37', '254739', '19ED3B']):
+#         newcol = input(f"Введите {el}, например #{col} или {col}: ")
+#         if not newcol.startswith("#"):
+#             newcol = f"#{newcol}"
+#         style.append(newcol)
+#
+#     # themename = "тестhex"
+#     # styles = [['rgba(65, 122, 166, 1)', 'rgba(30, 39, 53, 1)', 'rgba(255, 184, 89, 1)'],
+#     #           ['rgba(60, 45, 132, 1)', 'rgba(30, 39, 53, 1)', 'rgba(223, 205, 131, 1)'],
+#     #           ['#FF249E37', '#FF254739', '#FF19ED3B'],
+#     #           ['#249E37', '#254739', '#19ED3B']]
+#     # style = styles[3]
+#     savetheme(data, style)
+#     # for index, widgettype in enumerate(data['WidgetStyles']['$values']):
+#     #     data['WidgetStyles']['$values'][index]['Title']['Background']['Color']['Color'] = style[0]
+#     #     data['WidgetStyles']['$values'][index]['Background']['Color']['Color'] = style[1]
+#     #
+#     # data['WidgetStyles']['$values'][15]['FillColor'] = style[2]
+#     # data['Name'] = themename
+#     #
+#     #
+#     # with open(f'{themename}.json', 'w', encoding='utf-8') as fp:
+#     #     json.dump(data, fp, ensure_ascii=False, indent='\t')
+#     # f.close()
+# except Exception as e:
+#     print(e)
+#     time.sleep(5)
--- tkintercolors.py
+++ tkintercolors.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import colorchooser
-import json
-f = open('theme.json', encoding="utf-8")
-data = json.load(f)
+import json, themeform as tf
+# f = open('theme.json', encoding="utf-8")
+# data = json.load(f)
 
 root = Tk()
 root.title("Генератор тем для Visiology")
@@ -14,6 +14,7 @@ frame.pack(anchor=NW, side=LEFT)
 
 titlecol = "#34A2FE"
 fillcol = "#1E2735"
+graphscol = "#FFA700"
 lab = Label(frame, text="Заголовок", background=titlecol, fg="#FFFFFF", font=("Arial", 21, "bold"), width=13)
 lab.pack()
 # lab.grid(column=0, row=1)
@@ -23,27 +24,61 @@ lab2.pack()
 
 
 def colorbg():
-    titlecol = colorchooser.askcolor()[1]
+    entry = entrybg.get()
+    global titlecol
+    if entry:
+        if len(entry) > 6:
+            if entry.startswith("#"):
+                entry = entry[3:]
+            else:
+                entry = entry[2:]
+        if not entry.startswith("#"): entry = f"#{entry}"
+        titlecol = entry
+        entrybg.delete(0, END)
+    else:
+        titlecol = colorchooser.askcolor()[1]
     lab.config(bg=titlecol)
-    # my_label = tk.Label(root, text=my_color).pack(pady=10)
-    # my_label2 = tk.Label(root, text="You picked a color!", font=("Helvetica", 32), bg=my_color).pack()
-    # lab.config(bg=my_color)
+
 
 def colorfg():
     lab.config(fg=colorchooser.askcolor()[1])
 
+
 def colorbg2():
-    fillcol = colorchooser.askcolor()[1]
+    entry = entrybg2.get()
+    global fillcol
+    if entry:
+        if len(entry) > 6:
+            if entry.startswith("#"):
+                entry = entry[3:]
+            else:
+                entry = entry[2:]
+        if not entry.startswith("#"): entry = f"#{entry}"
+        fillcol = entry
+        entrybg2.delete(0, END)
+    else:
+        fillcol = colorchooser.askcolor()[1]
     lab2.config(bg=fillcol)
 
+
+def createtheme():
+    tf.savetheme(tf.jsonload(), [titlecol, fillcol, graphscol], entrytheme.get())
 
 frame2 = Frame(root)
 # frame2.grid(sticky="N", column=1, row=0)
 frame2.pack(anchor=NW)
-my_button = Button(frame2, text="Цвет заголовка", command=colorbg).grid(sticky="W", column=0, row=0)
+butbg = Button(frame2, text="Цвет заголовка", command=colorbg).grid(sticky="W", column=0, row=0)
+entrybg = Entry(frame2, width=10)
+entrybg.grid(sticky="W", column=1, row=0)
 # my_button2 = Button(frame2, text="Выбрать цвет текста", command=colorfg).grid(sticky="W", column=0, row=1)
-my_button3 = Button(frame2, text="Цвет фона", command=colorbg2).grid(sticky="W", column=0, row=1)
-my_button4 = Button(root, text="Создать тему", command=colorbg2).place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
+butbg2 = Button(frame2, text="Цвет фона", command=colorbg2).grid(sticky="W", column=0, row=1)
+entrybg2 = Entry(frame2, width=10)
+entrybg2.grid(sticky="W", column=1, row=1)
+
+entrytheme = Entry(root, width=40)
+entrytheme.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
+entrytheme.insert(0, "Название_темы")
+buttheme = Button(root, text="Создать тему", command=createtheme).place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
 
 
 root.mainloop()
